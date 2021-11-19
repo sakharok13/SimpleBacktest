07286498374f940e32a12ed559b6f566f8545a43
Alexander Gambashidze <58632360+sakharok13@users.noreply.github.com>
2021-11-19 20:04:05 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,8 +1,8 @@
 def strategy(price, usd, eth):
 
   if usd - eth*price > 0:
-    return ((usd - eth * price) / 2 , 0)
+    return (usd - eth * price) / 2
   elif usd - eth*price < 0:
-    return ( (usd - eth*price) / 2)
+    return  (usd - eth*price) / 2
   else:
     return(0)
